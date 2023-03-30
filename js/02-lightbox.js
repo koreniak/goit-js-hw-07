b8b84a7c2747730d8f6e1ffba7290e101572dd56
Borys Koreniak <koreniak.borys@gmail.com>
2023-03-30 17:41:29 +0300

    Виправ 2 завдання.

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,8 +6,6 @@ const lightboxMarkup = createLightboxItemsMarkup(galleryItems);
 
 lightboxRef.insertAdjacentHTML('beforeend', lightboxMarkup);
 
-lightboxRef.addEventListener('click', onLightboxImageClick)
-
 function createLightboxItemsMarkup(galleryItems) {
     return galleryItems.map(({preview, original, description}) => {
         return `
@@ -19,20 +17,7 @@ function createLightboxItemsMarkup(galleryItems) {
     }).join('');
 }
 
-
-
-function onLightboxImageClick (e) {
-    e.preventDefault();
-    
-    const isLightboxImageSwatch = e.target.classList.contains('gallery__image');
-
-    if (!isLightboxImageSwatch) {
-        return;
-    };
-
-    const gallery = new SimpleLightbox('.gallery a', {
-        captionDelay: 250,
-        captionsData: "alt",
-    });
-    gallery.show()
-}
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: "alt",
+});
